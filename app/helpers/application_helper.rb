02adf7ba505fc7d050c77c94877667580aad1be9
Bartosz <bartosz.logwiniuk@gmail.com>
2017-11-21 14:05:23 +0000

    Changed session source from view to helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,10 @@ module ApplicationHelper
     end
   end
   
+  def source_helper
+    if session[:source]
+      greeting = "Thanks for visiting me from  #{session[:source]}"
+      content_tag(:p, greeting, class: 'greeting') 
+    end
+  end
 end
